5e0f44a74b73f07ed1a6f157c12a456b9c22d5b0
canvameet <meetgaming43@gmail.com>
2026-09-19 23:31:20 +0530

    Add global error handler

--- admin-panel/src/main.jsx
+++ admin-panel/src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+window.addEventListener('error', (e) => {
+  document.body.innerHTML += `<div style="color:red; background:white; padding:20px; position:absolute; top:0; left:0; z-index:9999; font-size:16px;"><b>Error:</b> ${e.message}<br/>${e.filename}:${e.lineno}<br/><pre>${e.error?.stack}</pre></div>`;
+});
+
+window.addEventListener('unhandledrejection', (e) => {
+  document.body.innerHTML += `<div style="color:red; background:white; padding:20px; position:absolute; top:0; left:0; z-index:9999; font-size:16px;"><b>Unhandled Rejection:</b> ${e.reason?.message || e.reason}<br/><pre>${e.reason?.stack}</pre></div>`;
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
